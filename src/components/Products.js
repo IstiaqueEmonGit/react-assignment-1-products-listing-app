4e3684824c04fa0bd458f4f38db786987e8f0e1a
Emon <emon.bsnmc1453@gmail.com>
2022-11-09 00:00:18 +0600

    innit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,14 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product';
+
+export default function Products ({products}){
+    return (
+        <div className='products'>
+            {products.map((product) => 
+                <Product key={product.id} product={product} />
+            )}
+        </div>
+    );
+}
+
